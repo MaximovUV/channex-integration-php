ca62dbb833794e1d8e6085b243130eeaea08243e
Andrey Bashkirev <greed_nord@mail.ru>
2024-10-22 16:20:15 +0300

    add property user

---
+++ src/PropertyUsers.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ChannexIntegration;
+use ChannexIntegration\Channex;
+use ChannexIntegration\Init;
+final class PropertyUsers extends Channex {
+  public function __construct(Init $init) {
+      parent::__construct($init);
+  }
+
+  public function get(string $id = null) {
+        return $this->apiConnect->getApiInfo("GET", 'property_users/filter[property_id]='.(string)$id);
+  }
+
+  public function create(array $data):mixed {
+      return $this->apiConnect->getApiInfo("POST", 'property_users', ['invite' => $data]);
+  }
+
+  public function update(string $id, mixed $data):mixed {
+      return $this->apiConnect->getApiInfo("PUT", 'property_users/'.(string)$id, ['property_user' => $data]);
+  }
+
+  public function remove(string $id = null):mixed {
+      return $this->apiConnect->getApiInfo("DELETE", 'room_types/'.(string)$id);
+  }
+
+}
